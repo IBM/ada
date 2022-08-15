3ca8b706a24d4f480657ce46fe97ed0e4e706918
julia-farias <julia.farias@ibm.com>
2022-08-15 15:26:17 -0300

    Create retrieve_data_from_scheduling test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 import app
 from app import UnauthorizedException, ForbiddenException
+import unittest
 import pytest
-from unittest import mock
+from unittest.mock import MagicMock
 
 client = app.app.test_client()
 
@@ -11,8 +12,12 @@ client = app.app.test_client()
 #     response = client.get('/task_id/task_id')
 #     assert response.status_code == 200
 
-def test_authentication_layer():
-    token = None
-    with pytest.raises(UnauthorizedException):
-        app.authentication_layer()
-    assert token == None
+# def test_authentication_layer(mocker):
+#     mocker.patch.object(requests, "request", return_value=None)
+#     with pytest.raises(UnauthorizedException):
+#         app.authentication_layer()
+
+def test_retrieve_data_from_scheduling_error():
+	with pytest.raises(Exception) as excinfo:
+		app.retrieve_data_from_scheduling('anything', 'anything')
+	assert str(excinfo.value) == 'some info'
