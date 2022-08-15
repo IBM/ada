b649922abdf2285b8ac5bf75cfac48a2cb92fa8a
julia-farias <julia.farias@ibm.com>
2022-08-15 18:10:44 -0300

    Mock Fernet object

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from cryptography.fernet import InvalidToken
+from cryptography.fernet import Fernet, InvalidToken
 import app as application
 from app import UnauthorizedException, ForbiddenException
 import pytest
@@ -12,15 +12,15 @@ def test_authentication_layer_unauthorized():
         with application.app.test_request_context():
             application.authentication_layer()
 
+
 def test_authentication_layer_forbidden(mocker):
-    mocker.patch("app.authentication_layer", side_effect=ForbiddenException)
+    mocker.patch.object(Fernet, "__init__", side_effect=ForbiddenException)
     with pytest.raises(ForbiddenException):
         with application.app.test_request_context():
             application.authentication_layer()
 
+
 def test_invalid_token():
     with pytest.raises(InvalidToken):
-        with application.app.test_request_context(
-            headers={"api_key": "wrong_api_key"}
-        ):
+        with application.app.test_request_context(headers={"api_key": "wrong_api_key"}):
             application.authentication_layer()
