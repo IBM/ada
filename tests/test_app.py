f28d414fdcb8968fe012164a1ce89b8eaa52899a
julia-farias <julia.farias@ibm.com>
2022-08-16 18:53:12 -0300

    Add DatabaseError test

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,9 +2,15 @@ from cryptography.fernet import Fernet, InvalidToken
 import app as application
 from app import UnauthorizedException, ForbiddenException
 import pytest
+import psycopg2 as pg
+import pandas as pd
 from unittest import mock
 
 client = application.app.test_client()
+
+class PostgreMock:
+    def close(self):
+        return True
 
 
 def test_authentication_layer_success(mocker):
@@ -32,3 +38,10 @@ def test_authentication_layer_invalid_token():
         with application.app.test_request_context(headers={"api_key": "wrong_api_key"}):
             application.authentication_layer()
 
+
+def test_retrieve_data_from_scheduling_database_error(mocker):
+    mocker.patch.object(pg, "connect", return_value=PostgreMock())
+    mocker.patch.object(pd, "read_sql", side_effect=pg.DatabaseError)
+    with pytest.raises(pg.DatabaseError):
+        with application.app.test_request_context():
+            application.retrieve_data_from_scheduling(object_id="task_id", object_name="task_id")
