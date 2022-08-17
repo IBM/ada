99a79280f878fba9985f04401cc190766baf8999
julia-farias <julia.farias@ibm.com>
2022-08-17 17:44:37 -0300

    Add /dag_id exceptions tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -116,6 +116,47 @@ def test_task_id_invalid_token(mocker):
     })
     mocker.patch("app.authentication_layer", side_effect=InvalidToken)
     response = client.get("/task_id/test_task_id")
-    print(response)
+    assert response.status_code == 403
+    assert response.data.decode("utf-8") == expected_return
+
+
+def test_dag_id_success(mocker, mock_df):
+    expected_return = mock_df.to_json(orient='records')
+    mocker.patch("app.authentication_layer", return_value=True)
+    mocker.patch("app.retrieve_data_from_scheduling", return_value=mock_df)
+    response = client.get("/dag_id/test_dag_id")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8")  == expected_return
+
+
+def test_dag_id_unauthorized(mocker):
+    expected_return = json.dumps({
+        "Result": "Failure",
+        "Reason": "Missing API KEY.",
+    })
+    mocker.patch("app.authentication_layer", side_effect=UnauthorizedException)
+    response = client.get("/dag_id/test_dag_id")
+    assert response.status_code == 401
+    assert response.data.decode("utf-8") == expected_return
+
+
+def test_dag_id_forbidden(mocker):
+    expected_return = json.dumps({
+        "Result": "Failure",
+        "Reason": "Wrong API KEY.",
+    })
+    mocker.patch("app.authentication_layer", side_effect=ForbiddenException)
+    response = client.get("/dag_id/test_dag_id")
+    assert response.status_code == 403
+    assert response.data.decode("utf-8") == expected_return
+
+
+def test_dag_id_invalid_token(mocker):
+    expected_return = json.dumps({
+        "Result": "Failure",
+        "Reason": "Wrong API KEY.",
+    })
+    mocker.patch("app.authentication_layer", side_effect=InvalidToken)
+    response = client.get("/dag_id/test_dag_id")
     assert response.status_code == 403
     assert response.data.decode("utf-8") == expected_return
