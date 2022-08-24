d06d728ffcf96c87a6fd1dcd5b0e4ca4c81f06f3
julia-farias <julia.farias@ibm.com>
2022-08-24 15:32:33 -0300

    Lint code

--- tests/test_sql_query.py
+++ tests/test_sql_query.py
@@ -1,18 +1,26 @@
 import pytest
 from models import sql_query
-from models.templates.templates import queries
 
-#print("---->", globals())
 
 def test_build_query_success(mocker):
-	jinja_mock = mocker.patch("models.sql_query.JinjaSql", autospec=True)
-	j = jinja_mock.return_value
-	params = {
+    jinja_mock = mocker.patch("models.sql_query.JinjaSql", autospec=True)
+    j = jinja_mock.return_value
+    params = {
         "object_id": "object_id",
         "object_name": "object_name",
-        "query_select": "base_query"
+        "query_select": "base_query",
     }
-	query = "SELECT {{object_id}} FROM table WHERE {{object_id}} = {{object_name}}"
-	mocker.patch("models.templates.templates", return_value=query)
-	j.prepare_query.return_value = (query, params)
-	assert sql_query.build_query(**params) == j.prepare_query.return_value
+    query = {
+        "base_query": "SELECT {{object_id}} FROM table WHERE {{object_id}} = {{object_name}}"
+    }
+    response = "SELECT %s FROM table WHERE %s = %s", (
+        "object_id",
+        "object_id",
+        "object_name",
+    )
+    mocker.patch("models.templates.templates", return_value=query)
+    j.prepare_query.return_value = response
+    assert (
+        sql_query.build_query(**params)
+        == "SELECT object_id FROM table WHERE object_id = object_name"
+    )
